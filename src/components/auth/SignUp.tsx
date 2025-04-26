6e1757790bd29ac3ac506e4abfb87ad88dd7e83b
Sahan Chamara <sahanchamara456@gmail.com>
2025-04-27 00:36:12 +0530

    google Sign up button function

--- src/components/auth/SignUp.tsx
+++ src/components/auth/SignUp.tsx
@@ -6,9 +6,14 @@ import React, { useState } from 'react'
 import { Button } from '../ui/button'
 import { Input } from '../ui/input'
 import { Link } from 'react-router-dom'
+import { baseURL } from '@/lib/base-url'
 
 const SignUp = () => {
     const [activeTab, setActiveTab] = useState("login");
+
+    const handleClick = () => {
+        window.location.href = `${baseURL}/auth/google`;
+    };
 
     return (
         <div className="flex min-h-screen bg-slate-50 p-4 items-center justify-center">
@@ -102,7 +107,7 @@ const SignUp = () => {
                                 </div>
 
                                 <div className="grid grid-cols-2 gap-3">
-                                    <Button variant="outline" type="button" className="flex items-center justify-center gap-2">
+                                    <Button onClick={handleClick} variant="outline" type="button" className="flex items-center justify-center gap-2">
                                         <svg className="h-5 w-5 text-red-500" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor">
                                             <path d="M12.545,10.239v3.821h5.445c-0.712,2.315-2.647,3.972-5.445,3.972c-3.332,0-6.033-2.701-6.033-6.032s2.701-6.032,6.033-6.032c1.498,0,2.866,0.549,3.921,1.453l2.814-2.814C17.503,2.988,15.139,2,12.545,2C7.021,2,2.543,6.477,2.543,12s4.478,10,10.002,10c8.396,0,10.249-7.85,9.426-11.748L12.545,10.239z" />
                                         </svg>
